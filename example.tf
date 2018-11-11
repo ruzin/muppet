b308dede18e373ab4244133fd0a4c81dbb3ea4d3
Ruzin <ruzin.saleem@gmail.com>
2018-11-11 17:27:28 +0000

    added module output

--- example.tf
+++ example.tf
@@ -1,7 +1,13 @@
-module "prod-account" {
+#Module Call
+module "example_account" {
 source = "./module"
 workmail_org_id = "m-xxxxxxxxxxxxxxxxxxxx"
 workmail_account_domain = "your_workamil_domain.com"
 account_name = "example-name1"
 password = "Password1234!"
  }
+
+#Output
+output "aws_account_id" {
+  value = "${module.example_account.aws_account_id}"
+}
